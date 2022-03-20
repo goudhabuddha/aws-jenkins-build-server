f2aa771201697adea4c162207859baa33f6814c4
goudhabuddha <2770287+goudhabuddha@users.noreply.github.com>
2022-03-20 16:12:17 -0700

    formatted variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,19 @@
-variable route53_domain_name {
+variable "route53_domain_name" {
   type        = string
   description = "The domain"
-  default = "natebayaws.cloud"
+  default     = "natebayaws.cloud"
 }
 
-variable route53_zone_id {
+variable "route53_zone_id" {
   type        = string
   description = <<EOF
 The route53 zone id where DNS entries will be created. Should be the zone id
 for the domain in the var route53_domain_name.
 EOF
-  default = "Z08283091JJ6X8ITV69S6"
+  default     = "Z08283091JJ6X8ITV69S6"
 }
 
-variable jenkins_dns_alias {
+variable "jenkins_dns_alias" {
   type        = string
   description = <<EOF
 The DNS alias to be associated with the deployed jenkins instance. Alias will
@@ -22,26 +22,26 @@ EOF
   default     = "jenkins-controller"
 }
 
-variable vpc_id {
+variable "vpc_id" {
   type        = string
   description = "The vpc id for where jenkins will be deployed"
-  default = "vpc-4ff8bc37"
+  default     = "vpc-4ff8bc37"
 }
 
-variable efs_subnet_ids {
+variable "efs_subnet_ids" {
   type        = list(string)
   description = "A list of subnets to attach to the EFS mountpoint. Should be private"
-  default = ["subnet-082823527a66dfb03", "subnet-01ead1fb0854f8abc"] # private A, private B
+  default     = ["subnet-082823527a66dfb03", "subnet-01ead1fb0854f8abc"] # private A, private B
 }
 
-variable jenkins_controller_subnet_ids {
+variable "jenkins_controller_subnet_ids" {
   type        = list(string)
   description = "A list of subnets for the jenkins controller fargate service. Should be private"
-  default = ["subnet-082823527a66dfb03", "subnet-01ead1fb0854f8abc"] # private A, private B
+  default     = ["subnet-082823527a66dfb03", "subnet-01ead1fb0854f8abc"] # private A, private B
 }
 
-variable alb_subnet_ids {
+variable "alb_subnet_ids" {
   type        = list(string)
   description = "A list of subnets for the Application Load Balancer"
-  default = ["subnet-04e026aa71cda0f0f", "subnet-01ea73f369a51bcd5"] # public A, public B
+  default     = ["subnet-04e026aa71cda0f0f", "subnet-01ea73f369a51bcd5"] # public A, public B
 }
